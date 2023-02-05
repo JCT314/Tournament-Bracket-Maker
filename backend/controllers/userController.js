9bbbaba88038d95c955743a145f01e044ef82b8b
Isaiah Guardado <gisaiah416@gmail.com>
2023-02-05 14:30:00 -0800

    Added users to database

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,9 @@
 const User = require('../models/userModel.js')
 const asyncHandler = require('express-async-handler')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
-const registerUser = asyncHandler((req, res) => {
+const registerUser = asyncHandler(async(req, res) => {
     console.log(req.body)
     const {email, username, password} = req.body
 
@@ -10,9 +12,38 @@ const registerUser = asyncHandler((req, res) => {
         throw new Error('Please add all fields')
         //res.send('Not all data has been inputted')
     }
-    else{
-        res.status(200).json({message: 'Stored Successfully'})
+    
+    const userExist = await User.findOne({username})
+
+    if(userExist){
+        res.status(400)
+        throw new Error('User already Exists')
     }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const user = await User.create({
+        username,
+        email,
+        password: hashedPassword
+    })
+
+    if(user){
+        res.status(201).json({
+            _id: user.id,
+            username: user.username,
+            email: user.email
+        })
+    }
+
+    else{
+        res.status(400)
+        throw new Error('Invalid user Data')
+    }
+
+
+
 })
 
 module.exports = 
